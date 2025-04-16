d9f5bc8a25bd8a779e2e32603f4be31d01857029
keshavsoft <nknnkumar@yahoo.com>
2025-04-16 15:40:17 +0530

    only for WA vite mazer

---
+++ Factory.bat
@@ -0,0 +1,10 @@
+@REM call npm run ToFactory
+call npm run FromBranchQrCodes
+call npm run FromBranchVoucher
+
+call npm run WashingVoucher
+call npm run FromEntryQrCodes
+call npm run PressingQrCodes
+call npm run PressingVoucher
+call npm run CompletionVoucher
+call npm run CompletionQrCodes
